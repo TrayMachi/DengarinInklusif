7b7508eac6cafcb15bfcb09e8aa1c11b2e064916
TrayMachi <tristan.agra@ui.ac.id>
2025-07-24 15:21:29 +0700

    fix: theme context

--- app/routes/_page.tsx
+++ app/routes/_page.tsx
@@ -1,5 +1,5 @@
 import { Home } from "lucide-react";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from '~/components/context/theme-provider';
 import { Outlet, type LoaderFunctionArgs } from "react-router";
 import { Toaster } from "~/components/ui/sonner";
 
