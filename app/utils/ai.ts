0077b286c48a3f356e57ec73faea0dc82cb4549b
Carleano Ravelza Wongso <carleanoravel@gmail.com>
2025-07-24 18:31:36 +0700

    fix: service account

--- app/utils/ai.ts
+++ app/utils/ai.ts
@@ -1,6 +1,16 @@
 import { SpeechClient } from "@google-cloud/speech";
 import { TextToSpeechClient } from "@google-cloud/text-to-speech";
 import { GoogleGenAI } from "@google/genai";
+import fs from "fs";
+import path from "path";
+
+const rawCreds = process.env.GOOGLE_SERVICE_ACCOUNT;
+
+if (rawCreds) {
+  const credsPath = path.join("/tmp", "google-creds.json");
+  fs.writeFileSync(credsPath, rawCreds);
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = credsPath;
+}
 
 export const speechClient = new SpeechClient();
 export const ttsClient = new TextToSpeechClient();
